b37d9b45ea8c46b29c90d9781b610da320d4b17e
hailay143 <hailay143@gmail.com>
2021-04-14 10:31:16 +0200

    Initial commit

---
+++ src/main/java/com/newsapi/SpringWebNewsApi/SpringWebNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.newsapi.SpringWebNewsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebNewsApiApplication.class, args);
+	}
+
+}
